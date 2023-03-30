ed3346cf93ea972c728201ea4958173dd3b0551b
jdelaney1 <jdelaney@viqi.org>
2023-03-29 23:31:57 -0700

    first commit

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Three from "./three";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Three className="Three" />
+    </div>
+  );
+}
+
+export default App;
